22f305966ec6b17a721e268ff3616166ae9761b3
Ryan Frantz <ryanleefrantz@gmail.com>
2017-05-17 21:55:07 -0400

    Handles edited messages better

--- main.go
+++ main.go
@@ -53,77 +53,78 @@ func main() {
 			channel_id := channel_info.ID
 			channel_name := channel_info.Name
 			// TODO: Handle this case and log accordingly to Elasticsearch.
-			// This if statement needs to wrap the rest of the code in main().
+			// TODO: Break out bits of code below into more manageable functions.
 			if ev.SubType == "message_changed" {
 				fmt.Printf("Received MessageEvent for message which has been changed/edited! Is hidden? [%s] There is likely no user info available.\n", strconv.FormatBool(ev.Hidden))
 			}
 			user_info, err := rtm.GetUserInfo(ev.User)
 			if err != nil {
-				log.Fatal(err) // TODO: Handle 'user_not_found' case
-			}
-			if user_info.ID != bot_id { // We may not want to respond to our own bot and get in a loop.
-				re_bot_request := regexp.MustCompile("^<@" + bot_id + ">\\s+(\\w+)")
-				event_uuid := eventsByChannel[channel_name]
-				if re_bot_request.MatchString(ev.Text) == true {
-					is_command, bot_command, event_name, err := is_bot_command(ev.Text)
-					if err != nil {
-						// TODO: Log and return from this block.
-						fmt.Printf("Failed to check if '%s' is a bot command: %s\n", ev.Text, err)
-					}
-					if is_command {
-						fmt.Printf("COMMAND: '%s'; EVENT: '%s'\n", bot_command, event_name) // DEBUG
-						// TODO: Consider logging the bot command as a separate field.
-						switch bot_command {
-						case "start":
-							// Test for an existing event in eventsByChannel.
-							if event_uuid != "" {
-								rtm.SendMessage(rtm.NewOutgoingMessage("Already tracking an event in this channel", channel_id))
-							} else {
-								// Generate a UUID to tag messages.
-								event_uuid, err = Uuid()
-								if err != nil {
-									log.Fatal(err)
-								}
-								// TODO: Let's track the event name as well.
-								eventsByChannel[channel_name] = event_uuid
-							}
-						case "stop":
-							eventsByChannel[channel_name] = "" // Clear any event UUID.
+				fmt.Printf("Unable to get user information: %s\n", err) // 'user_not_found' when ev.SubType == "message_changed"
+			} else {
+				if user_info.ID != bot_id { // We may not want to respond to our own bot and get in a loop.
+					re_bot_request := regexp.MustCompile("^<@" + bot_id + ">\\s+(\\w+)")
+					event_uuid := eventsByChannel[channel_name]
+					if re_bot_request.MatchString(ev.Text) == true {
+						is_command, bot_command, event_name, err := is_bot_command(ev.Text)
+						if err != nil {
+							// TODO: Log and return from this block.
+							fmt.Printf("Failed to check if '%s' is a bot command: %s\n", ev.Text, err)
 						}
+						if is_command {
+							fmt.Printf("COMMAND: '%s'; EVENT: '%s'\n", bot_command, event_name) // DEBUG
+							// TODO: Consider logging the bot command as a separate field.
+							switch bot_command {
+							case "start":
+								// Test for an existing event in eventsByChannel.
+								if event_uuid != "" {
+									rtm.SendMessage(rtm.NewOutgoingMessage("Already tracking an event in this channel", channel_id))
+								} else {
+									// Generate a UUID to tag messages.
+									event_uuid, err = Uuid()
+									if err != nil {
+										log.Fatal(err)
+									}
+									// TODO: Let's track the event name as well.
+									eventsByChannel[channel_name] = event_uuid
+								}
+							case "stop":
+								eventsByChannel[channel_name] = "" // Clear any event UUID.
+							}
 
+						}
 					}
-				}
 
-				// golang's time doesn't parse epoch strings. Convert to int64 and do some magic.
-				intSize := 64
-				ts, err := strconv.ParseFloat(ev.Timestamp, intSize)
-				if err != nil {
-					fmt.Printf("Unable to convert timestamp '%s' to int64: %s\n", ev.Timestamp, err)
-				}
-				slack_time := time.Unix(int64(ts), 0) // TODO: Address failures above.
-				//now := time.Now()
-				// Slack often feeds us the last few messages in a channel when
-				// we join. Let's ignore those lest we accidentally duplicate
-				// stored messages or worse, enables/disables recording an
-				// event because it picked up an old command.
-				//if slack_time.Unix() < now.Unix() {
-				//    fmt.Printf("Slack time is older than now. Ignoring message! (%s)\n", ev.Text)
+					// golang's time doesn't parse epoch strings. Convert to int64 and do some magic.
+					intSize := 64
+					ts, err := strconv.ParseFloat(ev.Timestamp, intSize)
+					if err != nil {
+						fmt.Printf("Unable to convert timestamp '%s' to int64: %s\n", ev.Timestamp, err)
+					}
+					slack_time := time.Unix(int64(ts), 0) // TODO: Address failures above.
+					//now := time.Now()
+					// Slack often feeds us the last few messages in a channel when
+					// we join. Let's ignore those lest we accidentally duplicate
+					// stored messages or worse, enables/disables recording an
+					// event because it picked up an old command.
+					//if slack_time.Unix() < now.Unix() {
+					//    fmt.Printf("Slack time is older than now. Ignoring message! (%s)\n", ev.Text)
 
-				//} else {
-				edoc := ElasticsearchDocument{
-					ev.Timestamp,
-					slack_time.Format(time.RFC3339), // ISO 8601
-					channel_id,
-					channel_name,
-					user_info.ID,
-					user_info.Name,
-					ev.Text,
-					event_uuid,
+					//} else {
+					edoc := ElasticsearchDocument{
+						ev.Timestamp,
+						slack_time.Format(time.RFC3339), // ISO 8601
+						channel_id,
+						channel_name,
+						user_info.ID,
+						user_info.Name,
+						ev.Text,
+						event_uuid,
+					}
+					// TODO: Actually use err here.
+					es_json, _ := json.Marshal(edoc)
+					fmt.Println(string(es_json))                                                     // DEBUG
+					rtm.SendMessage(rtm.NewOutgoingMessage("Recorded "+string(es_json), channel_id)) // DEBUG
 				}
-				// TODO: Actually use err here.
-				es_json, _ := json.Marshal(edoc)
-				fmt.Println(string(es_json))                                                     // DEBUG
-				rtm.SendMessage(rtm.NewOutgoingMessage("Recorded "+string(es_json), channel_id)) // DEBUG
 			}
 
 		case *slack.PresenceChangeEvent:
